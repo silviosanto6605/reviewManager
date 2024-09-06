1d76828ac14ff0959ba3ee717f45623fa0090cb5
Silvio Santoriello <silviosanto6605@gmail.com>
2024-09-06 21:19:26 +0200

    Now with album adding!

--- routes/edit.js
+++ routes/edit.js
@@ -63,5 +63,51 @@ router.post('/', function(req, res) {
   db.close();
 });
 
+router.post('/add', function(req, res) {
+  console.log(req.body);
+  var db = new sqlite3.Database('data.db');
+
+  var cover = req.body.cover;
+  var name = req.body.name;
+  var artist = req.body.artist;
+  var date = req.body.date;
+  var rating = req.body.rating;
+  var possession = req.body.possession;
+  var genre = req.body.genre;
+
+  if (!cover || !name || !artist || !date || !rating || !possession || !genre) {
+    return res.status(400).send('Bad Request: Missing required fields');
+  }
+
+  // Fetch the highest current ID
+  db.get('SELECT MAX(ID) as maxId FROM Album', function(err, row) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    var newId = (row.maxId || 0) + 1; // Increment from the max ID or start at 1 if no records exist
+
+    // Insert the new album with the generated ID
+    db.run(`INSERT INTO Album (ID, Cover, Nome, Artista, Data, Voto, Genere, Possesso) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+      [newId, cover, name, artist, date, rating, genre, possession],
+      function(err) {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Internal Server Error');
+        }
+
+        // Redirect after adding the new album
+        res.redirect('/view');
+      });
+
+    db.close();
+  });
+});
+
+
+
+
+
 
 module.exports = router;
